1ecd40a721010177e4be399fef40178a27370168
kawamoto-yuta <kawamoto@mediowl.co.jp>
2022-06-24 12:40:24 +0900

    任意の変更

---
+++ app/pkg/controller/balances.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	// "fmt"
+	// "net/http"
+
+	// "strconv"
+
+	"github.com/gin-gonic/gin"
+	// "github.com/fuhiz/docker-go-sample/app/pkg/connecter"
+	// "github.com/fuhiz/docker-go-sample/app/pkg/model"
+	// "github.com/google/uuid"
+)
+
+type BalanceController struct{}
+
+// 変更
+
+func (self *DepositController) CreateBalance(c *gin.Context) {
+	// uuidObj, _ := uuid.NewUUID()
+	// BalanceID := uuidObj.String()
+
+	// newDeposit := model.NewDeposit(BalanceID, UserID, DepositID, WithdrawalID)
+	// deposit, err := model.CreateDeposit(connecter.DB(), newDeposit)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "deposit create failed"})
+	// 	return
+	// }
+
+	// c.JSON(http.StatusOK, gin.H{"deposit": deposit})
+}
